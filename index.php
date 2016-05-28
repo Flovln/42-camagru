662bfe9c57e598dbbcaa8cc5141bd915c0d4c461
Flovln <florent.violin@gmail.com>
2016-05-28 19:05:52 +0200

    index.php created

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php session_start();?>
+<!doctype <!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="Camagru">
+	<meta name="keywords" content="HTML, CSS, Javascript">
+	<meta name="author" content="Florent Violin">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<title>My Camagru</title>
+</head>
+<body>
+	<h1>Hello World</h1>
+</body>
+</html>
