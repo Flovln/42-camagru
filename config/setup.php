b58d7d51c3fb08639e5b27e0d782c7e6ad411480
Flovln <florent.violin@gmail.com>
2016-06-13 23:44:37 +0200

    database + creating tables OK

--- config/setup.php
+++ config/setup.php
@@ -11,7 +11,6 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
     // sql to create table
     $sql = "CREATE TABLE `Users` (
 	`id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -19,44 +18,34 @@ try {
 	`password` varchar(255) NOT NULL,
 	`email` varchar(255) NOT NULL
 	)";
+	$conn->exec($sql);
 
 	$sql = "CREATE TABLE `Images` (
 	`id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`user` varchar(255) NOT NULL,
 	`filepath` varchar(255) NOT NULL
 	)";
+	$conn->exec($sql);
 
 	$sql = "CREATE TABLE `Likes` (
 	`id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`user` varchar(255) NOT NULL,
 	`filepathimage` varchar(255) NOT NULL
 	)";
+	$conn->exec($sql);
 
 	$sql = "CREATE TABLE Comments (
 	`id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`user` varchar(255) NOT NULL,
 	`filepathimage` varchar(255) NOT NULL
 	)";
-
     // use exec() because no results are returned
     $conn->exec($sql);
     echo "Tables created successfully";
-    }
+}
+
 catch(PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
 }
 $conn = null;
-/*try {
-	// create connection
-	$db = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
-	// check connection
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//	$inject = "CREATE DATABASE";
-	$inject = "CREATE DATABASE db_camagru";
-	$db->exec($inject);
-}
-catch ( PDOException $Exception ) {
-	echo $Exception->getMessage();
-	die();
-}*/
 ?>
