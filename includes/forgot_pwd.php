2195ebc2e1ac5ddf4c9022d851da5303f7b294aa
Florent VIOLIN <fviolin@student.42.fr>
2017-04-04 14:44:07 +0200

    work in progress

--- includes/forgot_pwd.php
+++ includes/forgot_pwd.php
@@ -1,24 +1,15 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-      <title>Change your password</title>
-  </head>
-  <body>
-    <form action="change_password.php" method="POST">
-      Please enter your email:
-      <br /> 
-      <input type="email" name="email"/>
-      <br />
-      New password:
-      <br /> 
-      <input type="password" name="newpwd"/>
-      <br />
-      New password confirmation:
-      <br />
-      <input type="password" name="newpwd_confir"/>
-      <br />
-      <input type="submit" name="submit" value="OK"/>
-    </form>
-  </body>
-</html>
+<form action="change_password.php" method="POST">
+  Please enter your email:
+  <br /> 
+  <input type="email" name="email"/>
+  <br />
+  New password:
+  <br /> 
+  <input type="password" name="newpwd"/>
+  <br />
+  New password confirmation:
+  <br />
+  <input type="password" name="newpwd_confir"/>
+  <br />
+  <input type="submit" name="submit" value="OK"/>
+</form>
--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,9 @@
 <div id="header">
-<!--  <img src="pictures/Camagru_logo.png" title="camagru" id="logo"> -->
-  Camagru
+  <a href="index.php">Camagru</a>
   <? if (isset($_SESSION['auth'])) {
     echo "Welcome " . $_SESSION['login'];?>
     <div id="logout">
-      <? echo ("<li><a href=\"/camagru/includes/logout.php\">Logout</a></li>");?>
+      <? echo ("<li><a href=\"includes/logout.php\">Logout</a></li>");?>
     </div>
   <? } ?>
 </div>
