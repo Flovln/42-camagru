700840299ff2eed38f92432a9abb91a4e06a8280
Florent VIOLIN <fviolin@student.42.fr>
2017-04-06 18:40:08 +0200

    work on upload images

--- actions/upload.php
+++ actions/upload.php
@@ -28,7 +28,6 @@ $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
 // Check if image file is a actual image or fake image
-
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
@@ -41,21 +40,18 @@ if(isset($_POST["submit"])) {
 }
 
 // Check if file already exists
-
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
     $uploadOk = 0;
 }
 
 // Check file size
-
 if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
 
 // Allow certain file formats
-
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
@@ -63,17 +59,15 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
 }
 
 // Check if $uploadOk is set to 0 by an error
-
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
-
-// if everything is ok, try to upload file
-
 } else {
+// else if everything is ok, try to upload file
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
 }
+header('Location: ../index.php');
 ?>
