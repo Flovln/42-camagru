a1b7a21b8fab69c61ca33f87f9619654d09590ef
Florent VIOLIN <fviolin@student.42.fr>
2017-04-06 16:02:46 +0200

    work in progress

--- actions/save_picture.php
+++ actions/save_picture.php
@@ -6,18 +6,29 @@
 	}
 	$img = !empty($_POST['img_data']) ? $_POST['img_data'] : die("No image was taken");
 	//here we give a name to the image
-	$imgName = 'webcam.png';
+//	$imgName = 'webcam.png';
 	$img = str_replace('data:image/png;base64,', '', $img);
 	$img = str_replace(' ', '+', $img);
 	$fileData = base64_decode($img);
 
-//	$id = imagecreatefromstring(fileData);
-//	echo 'id content'.$id;
-//	$src = imagecreatefrompng($_POST['filter']);
-//	echo 'print src '.$src;
-	//saving
+	//src = filter to be applied 
+	$src = imagecreatefrompng('../filters/'.$_POST['filter']);
+	//dest = webcam picture
+	$dest = imagecreatefromstring($fileData);
+
+	// Get width and height of bottom layer
+	$dest_width = imagesx($dest);
+	$dest_height = imagesy($dest);
+
+	//apply filter on webcam picture
+	imagecopy($dest, $src, 0, 0, 0, 0, $dest_width, $dest_height);
+
+	//saving in directory
 	if (!file_exists('../user_imgs') && !is_dir('../user_imgs')) {
 		mkdir('../user_imgs');
 	}
-	file_put_contents('../user_imgs/'.$imgName, $fileData);
+	$imgPath = '../user_imgs/'.$_SESSION['login'].'/'.uniqid().'.png';
+	imagepng($dest, $imgPath);
+	imagedestroy($dest);
+//	file_put_contents('../user_imgs/'.$imgName, $fileData);
 ?>
