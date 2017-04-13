447f13afb8b8c6315478d9c6e16554f451e354d3
Florent VIOLIN <fviolin@student.42.fr>
2017-04-13 16:55:07 +0200

    work in progress

--- includes/sign_up.php
+++ includes/sign_up.php
@@ -1,24 +1,36 @@
 <?
 	include('../tools/state.php');
 ?>
-<div id="header">
-  <a href="../index.php">Camagru</a>
-  <? if (isset($_SESSION['auth'])) {
-    echo "Welcome " . $_SESSION['login'];?>
-    <div id="logout">
-      <? echo ("<li><a href=\"logout.php\">Logout</a></li>");?>
-    </div>
-  <? } ?>
-</div>
-<div align="center">
-	<form action="../actions/create_account.php" method="POST">
-	    Identifiant: <input type="login" name="login" required />
-	    <br/>
-	    Mot de passe: <input type="password" name="passwd" required />
-	    <br/>
-	    Email: <input type="email" name="email" required></input>
-	    <br/>
-	    <input type="submit" name="submit" value="OK" required />
-	</form>
-	<?display_error('err_signin')?>
-</div>
+<html>
+	<head>
+	  <meta charset="utf-8">
+	  <meta name="description" content="Camagru">
+	  <meta name="author" content="Florent Violin">
+	  <link rel="stylesheet" type="text/css" href="../style/main.css">
+	  <title>Camagru</title>
+	</head>
+	<body class="signup">
+		<div id="header">
+		  <a class="logo" href="../index.php">Camagru</a>
+		  <a class="gallery-link" href="gallery.php">Gallery</a>
+		  <? if (isset($_SESSION['auth'])) {
+		    echo "Welcome " . $_SESSION['login'];?>
+		    <div id="logout">
+		      <? echo ("<li><a href=\"logout.php\">Logout</a></li>");?>
+		    </div>
+		  <? } ?>
+		</div>
+		<div align="center">
+			<form action="../actions/create_account.php" method="POST">
+			    Identifiant: <input type="login" name="login" required />
+			    <br/>
+			    Mot de passe: <input type="password" name="passwd" required />
+			    <br/>
+			    Email: <input type="email" name="email" required></input>
+			    <br/>
+			    <input type="submit" name="submit" value="OK" required />
+			</form>
+			<?display_error('err_signin')?>
+		</div>
+	</body>
+</html>
